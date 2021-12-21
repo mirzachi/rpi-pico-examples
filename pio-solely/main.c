f30019d5e28d66159f624c1c9d9ba4f1a2344d02
mirzachi <mirzachi@gmail.com>
2021-12-21 21:33:38 +0100

    Fix timing issues and add log via USB

--- pio-solely/main.c
+++ pio-solely/main.c
@@ -2,14 +2,16 @@
 #include "hardware/pio.h"
 #include "hardware/dma.h"
 #include "hardware/clocks.h"
+#include <stdio.h>
 
 // PIO assembly code for the provider and consumer state machines
 #include "pio-provider-asm.pio.h"
 #include "pio-consumer-asm.pio.h"
 
 int main() {
-    // Set the system clock to 48 MHz and set the peripheral clock to match
-    set_sys_clock_48mhz();
+    stdio_init_all();
+    // Set the system clock to 100 MHz and set the peripheral clock to match
+    set_sys_clock_khz(100000, true);
 
     // Choose which PIO block to use
     // Both state machines will run on the same PIO block
@@ -33,17 +35,17 @@ int main() {
     uint dma_channel = 0;
 
     dma_channel_config c = dma_channel_get_default_config(dma_channel);
+    channel_config_set_transfer_data_size(&c, DMA_SIZE_32);
     channel_config_set_read_increment(&c, false); // Read always from the same address (FIFO)
     channel_config_set_write_increment(&c, false); // Write always to the same address (FIFO)
-    // Configure DMA timer (how often the data should be transferred)
-    dma_timer_set_fraction(0, 1, 48 * MHZ); // each second
-    channel_config_set_dreq(&c, dma_get_timer_dreq(0));
+    // Configure DMA pace rate to be adjusted to the consumer SM
+    channel_config_set_dreq(&c, pio_get_dreq(pio, sm_consumer, true));
 
     dma_channel_configure(dma_channel,
                           &c,
                           &pio->txf[sm_consumer], // destination address
                           &pio->rxf[sm_provider], // source address
-                          1000000, // number of transfers
+                          10000000, // number of transfers [limited]
                           true); // start immediately
 
     // Start programs on state machines
@@ -51,7 +53,9 @@ int main() {
     consumer_program_start(pio, sm_consumer);
 
     while (true) {
-        // TODO: read from SM2 FIFO and send via usb to be able to debug
-        sleep_ms(1000);
+        uint32_t element = pio_sm_get_blocking(pio, sm_consumer);
+        printf("%u\n", element);
+        //printf("%u\n", clock_get_hz(clk_sys));
+        sleep_ms(100);
     }
 }
